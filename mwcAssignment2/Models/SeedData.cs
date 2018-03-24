d6680ff861a2d69d3cf8271b3093242dac5e14d2
Bahit Hamid <bahit.hamid@gmail.com>
2018-03-24 09:48:57 +0800

    Added seeding model file to prepopulate the database. Only works if  database is empty.

---
+++ mwcAssignment2/Models/SeedData.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Linq;
+
+namespace mwcAssignment2.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new mwcAssignment2Context(
+                serviceProvider.GetRequiredService<DbContextOptions<mwcAssignment2Context>>()))
+            {
+                // Look for any movies.
+                if (context.Blog.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Blog.AddRange(
+                     new Blog
+                     {
+                         Author = "Bahit Hamid",
+                         Article = "This is the first article, incomplete and missing other fields. You'll need to modify SeedData.cs according to Assignment specs."
+                     }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
